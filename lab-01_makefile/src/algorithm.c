1f31be7f29cfa899f453876c4a6049bd3e4e8be7
what_a_draaag <dinara_valeeva_044@mail.ru>
2023-11-20 19:08:48 +0300

    first attempt

---
+++ lab-01_makefile/src/algorithm.c
@@ -0,0 +1,8 @@
+#include"io.h"
+#include"util.h"
+
+int doComputation(int a, double b) {
+  int c = max(3, 4);
+  logDebug('d');
+  return c;
+}
---
+++ lab-01_makefile/src/main.c
@@ -0,0 +1,10 @@
+#include"algorithm.h"
+#include"util.h"
+#include"io.h"
+
+int main() {
+  int a = doComputation(5, 7.0);
+  int b = 5;
+  max(a, b);
+  logDebug('!'); 
+}
