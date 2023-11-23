c408d3f1fb845ae5c7a783dd33c4e62aa823e894
what_a_draaag <dinara_valeeva_044@mail.ru>
2023-11-23 23:36:44 +0300

    first attempt

---
+++ lab-02_cstrings/include/str.h
@@ -0,0 +1,11 @@
+#ifndef STR_H
+#define STR_H
+
+#include <stddef.h>
+
+char* my_strcpy(char* destination, char* source);
+size_t my_strlen(char* str);
+char* my_strcat(char* destination, char* source);
+int my_strcmp(char* string1, char* string2);
+
+#endif
---
+++ lab-02_cstrings/src/test.c
@@ -0,0 +1,11 @@
+#include"test_str.h"
+
+int main()
+{
+	my_strcpy_test();
+	my_strcat_test();
+	my_strlen_test();
+	my_strcmp_test();
+	
+	return 0;
+}
