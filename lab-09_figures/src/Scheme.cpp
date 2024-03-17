1112b255da5c72e02df169e52bd9b7b485546c46
what_a_draaag <dinara_valeeva_044@mail.ru>
2024-03-17 10:44:34 +0300

    trying to fix 3.0

--- lab-09_figures/src/Scheme.cpp
+++ lab-09_figures/src/Scheme.cpp
@@ -3,55 +3,49 @@
 
 Scheme::Scheme(int capacity){
 	figures_ = new Figure*[capacity];
-	printf("capacity = %d\n", capacity);
+	//printf("capacity = %d\n", capacity);
 	capacity_ = capacity;
-	size = 0;
+	size_ = 0;
 }
 
 Scheme::~Scheme(){
-	for (int i=0;i<size; i++){
+	for (int i=0;i<size_; i++){
 		delete figures_[i];
 	}
 	delete [] figures_;
 }
 
 void Scheme::push_back_figure(Figure* fg){
-	if (size+1<=capacity_){
-		figures_[size] = fg;
-		size++;
-		printf("added figure with id %d at index %d\n", fg->getID(),size-1);
+	if (size_+1<=capacity_){
+		figures_[size_] = fg;
+		size_++;
+		//printf("added figure with id %d at index %d\n", fg->getID(),size_-1);
 	}
 }
 
 void Scheme::remove_figure(int id){
-	for (int i=0;i<size; i++){
-		if (figures_[i]->getID() == id){
-			delete figures_[i];
-			printf("removed figure by id %d\n", id);
-			size--;
-			for (int j=i;j<size;j++){
-				figures_[j] = figures_[j+1];
-			}
-		}
+	int i = get_index_by_id(id);
+	delete figures_[i];
+	//printf("removed figure by id %d\n", id);
+	for (int j=i;j<size_;j++){
+		figures_[j] = figures_[j+1];
 	}
+	size_--;
 }
 
 void Scheme::print_all_figures(){
-	for (int i=0;i<size; i++){
+	for (int i=0;i<size_; i++){
 		figures_[i]->print();
 	}
 }
 
 void Scheme::zoom_figure(int id, int factor){
-	for (int i=0;i<size; i++){
-		if (figures_[i]->getID() == id){
-			figures_[i]->zoom(factor);
-		}
-	}
+	int i = get_index_by_id(id);
+	figures_[i]->zoom(factor);
 }
 
 Figure* Scheme::is_inside_figure(int x, int y){
-	for (int i=0;i<size; i++){
+	for (int i=0;i<size_; i++){
 		if (figures_[i]->is_inside(x, y)){
 			return figures_[i];
 		}
@@ -60,9 +54,15 @@ Figure* Scheme::is_inside_figure(int x, int y){
 }
 
 void Scheme::move(int id, int new_x, int new_y){
-	for (int i=0;i<size; i++){
+	int i = get_index_by_id(id);
+	figures_[i]->move(new_x, new_y);
+}
+
+int Scheme::get_index_by_id(int id){
+	for (int i=0;i<size_; i++){
 		if (figures_[i]->getID() == id){
-			figures_[i]->move(new_x, new_y);
+			return i;
 		}
 	}
+	return -1;
 }
