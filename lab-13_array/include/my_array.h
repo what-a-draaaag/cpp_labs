092b4c0e67800c08cbbc032a3f7a40b8627e6249
what_a_draaag <dinara_valeeva_044@mail.ru>
2024-04-14 16:07:33 +0300

    not an attempt at all

--- lab-13_array/include/my_array.h
+++ lab-13_array/include/my_array.h
@@ -1,22 +1,59 @@
-#ifndef MY_ARRAY_H_
-#define MY_ARRAY_H_
+#pragma once
 
 #include <cstddef>
+#include <stdexcept>
 
 namespace lab_13 {
 
 template<typename T, std::size_t N>
 class my_array {
 public:
-    at(std::size_t index);
-    operator[](std::size_t index);
 
-    bool empty();
-    size();
+    my_array() = default;
+    my_array(const my_array& other){
+        for (int i=0; i<N; i++){
+            data[i] = other.data[i];
+        }
+    }
 
-    fill(T val);
+    T& at(std::size_t index){
+        if (index>=N){
+            throw std::out_of_range("Index out of range.");
+        }
+        return data[index];
+    }
+
+    T& operator[](std::size_t index){
+        return data[index];
+    }
+
+    const T& at(std::size_t index) const{
+        if (index>=N){
+            throw std::out_of_range("Index out of range.");
+        }
+        return data[index];
+    }
+
+    const T& operator[](std::size_t index) const{
+        return data[index];
+    }
+
+    bool empty() const{
+        return N == 0;
+    }
+    size_t size() const{
+        return N;
+    }
+
+    void fill(T val){
+        for (size_t i = 0; i< N; i++){
+            data[i] = val;
+        }
+    }
+
+private:
+    T data[N]{};
 };
 
 }  // namespace lab_13
 
-#endif  // MY_ARRAY_H_
--- lab-13_array/src/main.cpp
+++ lab-13_array/src/main.cpp
@@ -1,5 +1,4 @@
 #include "my_array.h"
-#include <cstddef>
 
 using lab_13::my_array;
 
