c17554fa43b76e217c4a3237826483b8d3d18fb4
what_a_draaag <dinara_valeeva_044@mail.ru>
2024-04-14 22:20:41 +0300

    first attempt

--- lab-13_array/include/my_array.h
+++ lab-13_array/include/my_array.h
@@ -55,5 +55,95 @@ private:
     T data[N]{};
 };
 
+
+class proxy_bool{
+public:
+    proxy_bool(int index, char& byte): index(index), byte(byte) {}
+
+    operator bool() const{
+        if (byte>>index){
+            return 1;
+        }
+        return 0;
+    }
+    proxy_bool& operator=(const proxy_bool& other){
+        *this = bool(other);
+        return *this;
+    }
+
+    proxy_bool& operator=(bool value){
+        if (value){
+            byte |= 1<<index;
+        }
+        else{
+            byte &= ~(1<<index);
+        }
+        return *this;
+    }
+
+private:
+    int index;
+    char& byte;
+};
+
+
+
+
+template <std::size_t N>
+class my_array<bool, N>{
+public:
+    my_array() = default;
+
+    my_array(const my_array& other){
+        for (int i=0; i<N; i++){
+            data[i] = other.data[i];
+        }
+    }
+
+    proxy_bool operator[](std::size_t index){
+        return proxy_bool(index%8, data[index/8]);
+    }
+
+    proxy_bool at(std::size_t index){
+        if (index>=N){
+            throw std::out_of_range("Index out of range.");
+        }
+        return (*this)[index];
+    }
+
+    bool operator[](std::size_t index) const{
+        return (data[index/8]>>index)?1:0;
+    }
+
+    bool at(std::size_t index) const{
+        if (index>=N){
+            throw std::out_of_range("Index out of range.");
+        }
+        return (*this)[index];
+    }
+
+    bool empty() const{
+        return N == 0;
+    }
+    size_t size() const{
+        return N;
+    }
+
+    void fill(bool val){
+        for (size_t i = 0; i< N/8; i++){
+            data[i] = val? -1 : 0;
+        }
+    }
+
+    friend proxy_bool;
+    
+private:
+    char data[N/8 + (N%8)/8]{};
+};
+
+
+
+
+
 }  // namespace lab_13
 
