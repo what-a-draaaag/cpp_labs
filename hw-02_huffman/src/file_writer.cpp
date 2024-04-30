b0af7cad51e9c00ecdda42b0ef58eb06de92cd0f
what_a_draaag <dinara_valeeva_044@mail.ru>
2024-05-01 02:28:42 +0300

    fixed is_useful (I hope so)

--- hw-02_huffman/src/file_writer.cpp
+++ hw-02_huffman/src/file_writer.cpp
@@ -40,10 +40,15 @@ bool file_writer::is_useful(huffman_tree& ht, huffman_tree::Table& table,  std::
 	int sizes_values_size = 2*sizeof(unsigned int);
 	int encoded_data_size = (encode_data(data, table).size()-1)/8+1;
 	if (data_size<= (table_size+sizes_values_size+encoded_data_size)){
-		ht.statistics.push_back(0);
 		ht.statistics.push_back(data_size);
+		ht.statistics.push_back(0);
+		for (char ch: data){
+			out.put(ch);
+		}
 		return false;
 	}
+	ht.statistics.push_back(encoded_data_size);
+	ht.statistics.push_back(table_size+sizes_values_size);
 	return true;
 }
 
@@ -52,7 +57,6 @@ void file_writer::write_table(huffman_tree& ht, huffman_tree::Table& table){
 	unsigned int size_of_table = table.size();
 	out.write(reinterpret_cast<char const*>(&size_of_table), 4);
 	unsigned int size_in_bytes = (size_of_table-1)/8 +1;
-	ht.statistics.push_back(size_in_bytes + sizeof(unsigned int)*2);
 	for (unsigned int i = 0; i< size_in_bytes; i++){
 		uint8_t byte = get_byte(table.bits, i);
 		out.put(byte);
@@ -64,7 +68,6 @@ void file_writer::write_data(huffman_tree& ht, std::vector<char>& data, huffman_
 	unsigned int size_of_data = encoded_data.size();
 	out.write(reinterpret_cast<const char*>(&size_of_data), 4);
 	unsigned int size_in_bytes = (size_of_data-1)/8 +1;
-	ht.statistics.push_back(size_in_bytes);
 	for (unsigned int i = 0; i< size_in_bytes; i++){
 		uint8_t byte = get_byte(encoded_data, i);
 		out.put(byte);
